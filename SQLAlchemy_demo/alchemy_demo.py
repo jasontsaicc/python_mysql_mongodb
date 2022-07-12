645676c4f1faf9777c34e380dc7e9d337418cd17
jason tsai <jason.tsaicc@gmail.com>
2022-07-12 20:16:15 +0800

    建立 session, insert 數據的插入

--- SQLAlchemy_demo/alchemy_demo.py
+++ SQLAlchemy_demo/alchemy_demo.py
@@ -34,8 +34,26 @@ class User(BASE):
 # 使用sa.create_engine設定數據庫的連接信息
 engine = sa.create_engine('mysql+pymysql://root:00065638@localhost:3306/demo')
 # 定義session 可以去連接數據庫 用來插入數據, 查詢數據
-session = sa.orm.sessionmaker(bind=engine)
+Session = sa.orm.sessionmaker(bind=engine)
 
 # 通過調用Base裡面的metadata.create_all()方法把engine傳入去建立數據庫
 # 會把BASE CLASS的子類(現在是user) 建立到數據庫裡面
 BASE.metadata.create_all(engine)
+
+# insert 數據 插入一個對象(User)的實例
+user1 = User(username='test1', password='test1', email='test1@test1.com')
+user2 = User(username='test2', password='test2', email='test2@test1.com')
+user3 = User(username='test3', password='test3', email='test3@test1.com')
+
+# 建立一個session
+session = Session()
+# 有了session後 就可以調用session.add()去插入數據
+
+# 插入單筆數據
+# session.add(user1)
+
+# 插入多筆數據
+session.add_all([user1, user2, user3])
+
+# 提交數據
+session.commit()
